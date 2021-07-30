fc4a7dd1bfa8d62800652918cc8f946e62e38923
sankari-chelliah <85871590+sankari-chelliah@users.noreply.github.com>
2021-07-30 06:48:25 -0400

    Add files via upload

---
+++ var2.py
@@ -0,0 +1,17 @@
+my_name = 'Sankari'
+my_age = 35
+my_height = 5.5 #feet
+my_weight = 168 #pounds
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"She is {my_height} feet tall.")
+print(f"She is {my_weight} pounds heavy.")
+print("Actually thats not too heavy")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth}.")
+
+total = my_age + my_height + my_weight
+print(f"If i add {my_age},{my_height} and {my_weight} i get {total}.")
