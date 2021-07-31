b7edb18fbb0655108a7d8dfa84aebe56db57a901
sankari-chelliah <85871590+sankari-chelliah@users.noreply.github.com>
2021-07-31 10:56:32 -0400

    Add files via upload

---
+++ mod_example.py
@@ -0,0 +1,7 @@
+from sys import argv
+script, first, second, third = argv
+
+print (f"your script is {script}")
+print (f"your first arg is {first}")
+print (f"your second arg is {second}")
+print ("your third arg is " + third)
