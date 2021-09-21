cdc051746f374fe62aa1faa42108c0b5d88b8395
sankari-chelliah <85871590+sankari-chelliah@users.noreply.github.com>
2021-09-21 10:21:41 -0400

    Update count_str_pattern.py

--- count_str_pattern.py
+++ count_str_pattern.py
@@ -1,5 +1,11 @@
 # Finding number of occurences of bob
 s= str(input("Enter string: "))
 s=s.lower()
-a=s.split("bob")  #Split input string into an array with separator bob
-print(f"Number of times bob occurs is: {(len(a)-1)}")
+
+#method1
+bob_count = s.count("bob")
+print(f"Number of times bob occurs is: {bob_count}")
+
+#method2
+#a=s.split("bob")  #Split input string into an array with separator bob
+#print(f"Number of times bob occurs is: {(len(a)-1)}")
